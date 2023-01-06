e3eb150b1086bf3890c149acd51a541fe75cbf49
yves makwambala <yvesmak1@gmail.com>
2023-01-06 16:32:30 +0100

    Finalisation du site javascript api

--- js/movie.js
+++ js/movie.js
@@ -1,7 +1,7 @@
 const searchForm = document.getElementById("searchForm");
 const searchInput = document.getElementById("searchInput");
 const result = document.getElementById("result");
-const key = "xxxxxx";
+const key = "xxxx";
 
 let search = "";
 let movies = [];
@@ -27,16 +27,25 @@ const mobiesDisplay = async ()=>{
             <p><iframe width="950" height="534" src="https://www.youtube.com/embed/xxx" title="All Nassr vs Al-Ta'i 5-1Goal and assist Ronaldo - Extеndеd Hіghlіghts & All Gоals 2022 HD" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
 
     */
-    result.innerHTML = movies.results.map((movie) =>
+    result.innerHTML = movies.results.map((movie) => 
     `
-    <li>
-        <h2>${movie.original_title}</h2>
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
-        <p>${movie.overview}</p>
-        <p>${movie.popularity}</p>
-        <p>Date de sortie = ${movie.release_date}</p>
-    </li>
-    <hr>
+    <article class="col-lg-12">
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="img-fluid rounded-start" alt="${movie.original_title}">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h1 class="card-title">${movie.original_title}</h1>
+                        <p class="card-text">${movie.overview}</p>
+                        <p class="card-text"><img src="./images/etoile.png" width="25"  alt="${movie.original_title}"> <small class="text-muted">Popularité : ${movie.popularity} </small></p>
+                        <p class="card-text"><img src="./images/date.png" width="25"  alt="${movie.original_title}"> <small class="text-muted">Date de sortie : ${movie.release_date} </small></p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </article>
     `
     ).join("");
 }
@@ -47,5 +56,3 @@ searchForm.addEventListener("submit",(e)=>{
     search = searchInput.value;
     mobiesDisplay();
 });
-
-
